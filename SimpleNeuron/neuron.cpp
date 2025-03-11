232a2eba5fdfa6fbf1ef797bcbebbd60bd26ebca
RiccardoRevalor <riccardorevalor@gmail.com>
2025-03-11 17:59:21 +0100

    fixed code for simple Neuron

--- SimpleNeuron/neuron.cpp
+++ SimpleNeuron/neuron.cpp
@@ -3,6 +3,7 @@ Implementation of a simple neuron to understand the NN from the ground up
 */
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -11,20 +12,14 @@ class Neuron {
 
     //the construct of the neuron receives the input array, the weights array and the bias and the number of inputs
     private:
-        double inputs[];
-        double weights[];
+        vector<double> inputs;
+        vector<double> weights;
         double bias;
         double output;
-        int numInputs;
 
     public:
-        Neuron(double inputs[], double weights[], double bias, int numImputs){
-            this.inputs = inputs;
-            this.weights = weights;
-            this.bias = bias;
-            //set the output of the neuron
-
-        }
+        Neuron(const vector<double> &inputs, const vector<double> &weights, double bias)
+            : inputs(inputs), weights(weights), bias(bias) {}
 
         //function to calculate the output of the Neuron
         double GetOutput(){
@@ -32,33 +27,37 @@ class Neuron {
             //iterate over the inputs and weights to calculate the output
             //at every iteration i, calculate inputs[i] * weights[i] and add it to the accumulator
 
-            for (int i = 0; i < this->numInputs; i++){
+            for (int i = 0; i < this->inputs.size(); i++){
                 acc += this->inputs[i] * this->weights[i];
             }
 
             //then add the bias to the accumulator
             return acc + this->bias;
         }
-}
+};
 
 
 //Main function to test the neuron
 int main(void) {
 
-    std::cout << "Simple Neuron" << std::endl;
+    cout << "Simple Neuron" << std::endl;
 
     //create inputs weight bias
-    double inputs[] = {1, 2, 3};
-    double weights[] = {0.2, 0.8, -0.5};
+    vector<double> inputs = {1, 2, 3};
+    vector<double> weights = {0.2, 0.8, -0.5};
     double bias = 2.0;
 
     //Create the neuron
-    Neuron neuron = new Neuron(inputs, weights, bias, 3);
+    Neuron neuron = Neuron(inputs, weights, bias);
 
     //Retrieve the output of the neuron
     double output = neuron.GetOutput();
 
-    std::cout << "Neuron generated this output: " << output << std::endl;   
+    cout << "Neuron generated this output: " << output << endl;   
+
+
+    cout << "Enter to exit...";
+    cin.get();
 
     return 0;
 }
