a3ae65b92e5a34a8a0d8a1d21490aeb407cb3076
RiccardoRevalor <riccardorevalor@gmail.com>
2025-03-11 21:33:28 +0100

    dot product more efficient than loop

--- LayerOfNeurons/neuron.cpp
+++ LayerOfNeurons/neuron.cpp
@@ -4,7 +4,14 @@ Implementation of a simple neuron to understand the NN from the ground up
 
 #include "neuron.h" //Neuron header defined in neuron.h
 
+#include <numeric>  //used for inner_product
+
 using namespace std;
+
+//Dot Production is more efficient than the for loop -> this is like the dot product in numpy
+double dotProduct(const vector<double> &v1, const vector<double> &v2){
+    return inner_product(v1.begin(), v1.end(), v2.begin(), 0.0);
+}
 
 //Neuron constructor
 Neuron::Neuron(const vector<double> &weights, double bias)
@@ -19,9 +26,16 @@ double Neuron::getOutput(const vector<double> &inputs){
     //iterate over the inputs and weights to calculate the output
     //at every iteration i, calculate inputs[i] * weights[i] and add it to the accumulator
 
+
+    /* Before using dot product
     for (size_t i = 0; i < inputs.size(); i++){
-        acc += inputs[i] * Neuron::weights[i];
+        acc += inputs[i] * Neuron::weights[i];    
+
     }
+    */
+
+    //use dot product to calculate the output in a more efficient way
+    acc = dotProduct(inputs, Neuron::weights);
 
     //then add the bias to the accumulator
     return acc + Neuron::bias;
